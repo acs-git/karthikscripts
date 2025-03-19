68c74559e4cb3644ece30ab7a737ae39c176fbd1
karthikdhanusu <36257249+karthikdhanusu@users.noreply.github.com>
2025-03-19 20:25:24 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,27 @@
+import requests
+
+baseurl = "https://core.labplatform.vmware.com"
+
+def login():
+    encoded_string = {'base64encodestring'}   #modify this line with the actual base64encode string
+    loginurl = baseurl+"/api/login"
+    headers = {
+        "Authorization": f"Basic {encoded_string}"
+    }
+    loginreq = requests.post(url=loginurl, headers=headers)
+    accountsurl = baseurl+'/api/admin/accounts?tenant=acs-labs&accountVerified=False&limit=100'
+    neetokhdr = {
+        'Cookie': loginreq.headers['Set-Cookie']
+    }
+    accountsreq = requests.get(url=accountsurl, headers=neetokhdr)
+    for i in accountsreq.json()['data']:
+        if '@ascendcloudsolutions.com' in i['username']:
+            i['accountVerified']=True
+            updateaccurl = baseurl+f'/api/admin/accounts?tenant=acs-labs&username={i['username']}'
+            updatereq = requests.put(url=updateaccurl, headers=neetokhdr, json=i)
+            print(updatereq.json())
+
+
+
+if __name__ == '__main__':
+    login()
